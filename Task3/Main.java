146908102e41b81f5c9706f96059ec7a0434e267
alaminhossainrifat <rif420aat@gmail.com>
2024-10-15 21:27:59 +0600

    add initial Balance from user

--- Task3/Main.java
+++ Task3/Main.java
@@ -1,9 +1,18 @@
 package Task3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        BankAccount userAccount = new BankAccount(1000.00); 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter initial balance: ");
+        double initialBalance = scanner.nextDouble();
+
+        BankAccount userAccount = new BankAccount(initialBalance); 
         ATM atm = new ATM(userAccount);
         atm.showMenu();
+        scanner.close();
     }
+    
 }
